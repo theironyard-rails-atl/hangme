a1a80270f21091e6dfbc8ad4691ef97ea5399023
James Dabbs <jamesdabbs@gmail.com>
2014-08-15 14:03:38 -0400

    Additions before review

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,4 +8,23 @@ class GamesController < ApplicationController
     id = params[:id]
     @game = Game.find(id)
   end
+
+  def create
+    random_word = File.read(__FILE__).
+      split(" ").reject{ |word| word.length < 5 }.sample
+
+    game = Game.create!(
+      max_misses: 10,
+      misses:     0,
+      answer:     random_word,
+      guessed:    ""
+    )
+    redirect_to game
+  end
+
+  def update
+    game = Game.find params[:id]
+    game.guess params[:guess]
+    redirect_to game
+  end
 end
